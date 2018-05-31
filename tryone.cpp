3ef15d04c5cb72e15c8fa064d226aad56f3ef9d1
Lucas Rodrigues <34749742+RodriguesLucas@users.noreply.github.com>
2018-05-31 12:49:56 -0300

    Add files via upload

---
+++ tryone.cpp
@@ -0,0 +1,29 @@
+// feito por: Lucas Rodrigues
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void menu(){ // função pra ficar mostrando o menu
+	cout << "************** Cadastro de aluno ENE **************" << endl;
+	cout << "1 - Inclusão aluno\n";
+	cout << "2 - Exclusão aluno\n";
+	cout << "3 - Pesquisa aluno\n";
+	cout << "4 - Alteração aluno\n";
+	cout << "5 - Listagem alunos\n";
+	cout << "6 - FIM\n";
+}
+
+
+int main(){
+	// declaração de variáveis
+	int i, j, cont, aux, chc;
+	
+	
+	setlocale(LC_ALL, "Portuguese"); // acentuação
+	menu();
+	
+	
+	return 0;
+}
